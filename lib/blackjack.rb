522fb15c7723e91ffad4163d47d33408c0adcc1f
Ruel Tingling <rvtingling@yahoo.com>
2019-09-23 14:44:55 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,24 +1,28 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+deal_card = random(1, 11)
+puts "deal_card"
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total()
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
+  puts ""Type 'h' to hit or 's' to stay""
   # code #prompt_user here
 end
 
 def get_user_input
+  gets.chomp
   # code #get_user_input here
 end
 
 def end_game
+  puts "sorry, you hit #{card_total}. Thanks for playing!"
   # code #end_game here
 end
 
@@ -27,6 +31,7 @@ def initial_round
 end
 
 def hit?
+   puts "Type 'h' to hit"
   # code hit? here
 end
 
@@ -39,6 +44,18 @@ end
 #####################################################
 
 def runner
+  Welcome
+  deal_card
+  display_card_total
+  prompt_user
+  get_user_input
+  end_game
+  initial_round
+  hit
+  invalid_command
+  
+  
+  
   # code runner here
 end
     
